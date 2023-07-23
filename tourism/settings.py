6168496d75c697f02286280b889ab2f0e363017a
sembe <salumrashid@gmail.com>
2023-07-23 03:41:46 +0300

    updates

--- tourism/settings.py
+++ tourism/settings.py
@@ -80,13 +80,23 @@ WSGI_APPLICATION = 'tourism.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'abumariam',
+        'USER': 'admin',
+        'PASSWORD': '2Salumsembe(',
+        'HOST': 'database-2.csmbvngaug01.eu-north-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
